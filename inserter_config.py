add591843a83eb6bead0d17695a4844113ab3b12
vdashkov <vdashkov@news360.com>
2019-02-24 08:59:38 -0800

    chanded default value for inserter; minor changes to selecter configs setup

--- inserter_config.py
+++ inserter_config.py
@@ -6,7 +6,7 @@ import sys
 HOST = os.getenv("HOST", "localhost")
 TABLE_NAME = os.getenv("TABLE_NAME")
 BULK_SIZE = int(os.getenv("BULK_SIZE", 1))
-EVENTS_PER_DAY = int(os.getenv("EVENTS_PER_DAY", 10))
+EVENTS_PER_DAY = int(os.getenv("EVENTS_PER_DAY", 100000))
 WORKERS = int(os.getenv("WORKERS", 10))
 DB_NAME = os.getenv("DB_NAME")
 HAS_DATE_COLUMN = False if int(os.getenv("HAS_DATE_COLUMN", 0)) == 0 else True
--- selecter_config.py
+++ selecter_config.py
@@ -1,5 +1,5 @@
+import os
+
 NUMBER_BENCHMARK_RUNS = 3
 
-QUERY = """
-SELECT sumMerge(types_sum) FROM aggregating.event_aggs
-"""
+QUERY = os.getenv("QUERY")
